296acd764465c82ef77b4800abe73301087ad7bc
eddys <eddygreyrat@gmail.com>
2024-09-22 14:54:49 -0700

    updated mem file's name

--- src/core/1_fetch_stage/instruction_mem.sv
+++ src/core/1_fetch_stage/instruction_mem.sv
@@ -6,21 +6,21 @@
 `include "../common/isa.svh"
 
 module InstructionMem(
-    input var logic [31:0] pc,
+    input var logic [29:0] pc,
     output var logic [31:0] instruction
 );
 
     (* ram_style = "block" *) var logic [7:0] instruction_memory [31:0];
     
     initial begin
-        $readmemh("C:/Users/sanch/Documents/FPGA_Programming/RISC-V_32IM/src/instructions.mem", instruction_memory); // Load BRAM from .mem file
+        $readmemh("C:/Users/sanch/Documents/FPGA_Programming/RISC-V_32IM/src/ROM.mem", instruction_memory); // load binary
     end
 
     assign instruction = {
-        instruction_memory[{pc[4:2], 2'b11}], 
-        instruction_memory[{pc[4:2], 2'b10}], 
-        instruction_memory[{pc[4:2], 2'b01}], 
-        instruction_memory[{pc[4:2], 2'b00}]
+        instruction_memory[{pc, 2'b11}], 
+        instruction_memory[{pc, 2'b10}], 
+        instruction_memory[{pc, 2'b01}], 
+        instruction_memory[{pc, 2'b00}]
     };
 
 endmodule
